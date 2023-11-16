a411f0defff4783d5a945611e0b67065eb58be6c
yuma <ikeoku.y@gmail.com>
2023-11-16 21:13:28 +0900

    繰り返し二乗法でとく

---
+++ tessoku-book/chapter5/b_29.rb
@@ -0,0 +1,7 @@
+MOD = 1000000007
+a, b = gets.split.map(&:to_i)
+
+n = a
+res = [a] + (0..60).map { |i| n *= n % MOD }
+
+p b.digits(2).map.with_index { |n, i| n == 1 ? res[i] : 1}.inject(1) { |acc, n| acc = (acc * n) % MOD }
