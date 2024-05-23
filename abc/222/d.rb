4420acee660aabb9627ddc2af17b9190ac6a242b
yuma <ikeoku.y@gmail.com>
2024-05-23 20:13:29 +0900

    間違ってた箇所を修正

--- abc/222/d.rb
+++ abc/222/d.rb
@@ -11,11 +11,35 @@ n.times do |i|
   k = 0
   s = dp.map { k += _1 }
   m.times do |j|
-    if a[i] <= j && j <= b[i]
-      dp[j] = s[j] % MOD
-    else
-      0
-    end
+    dp[j] =
+      if a[i] <= j && j <= b[i]
+        s[j] % MOD
+      else
+        0
+      end
+  end
+end
+
+p dp.sum { _1 % MOD } % MOD
+
+
+__END__
+三項演算子バージョン特に意味はない
+
+MOD = 998244353
+n = gets.to_i
+a = gets.split.map(&:to_i)
+b = gets.split.map(&:to_i)
+m = 3001
+
+dp = Array.new(m, 0)
+dp[0] = 1
+
+n.times do |i|
+  x = 0
+  s = dp.map { x += _1 }
+  m.times do |j|
+    dp[j] = a[i] <= j && j <= b[i] ? s[j] % MOD : 0
   end
 end
 
