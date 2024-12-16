39e7e28b41bc81f51f4fa51099491d6d1fa4c7eb
yuma <ikeoku.y@gmail.com>
2024-12-16 22:51:29 +0900

    Square869120Contest #6 B - AtCoder Marketsを解いた

--- kakomon_100_cyukyu/8.rb
+++ kakomon_100_cyukyu/8.rb
@@ -8,7 +8,6 @@ mid_b = b.sort[n/2]
 p a.sum { (_1 - mid_a).abs } + b.sum { (_1 - mid_b).abs } + ab.sum { |a, b| (a - b).abs }
 
 
-
 __END__
 入口の座標 s 出口の座標 t とする。
 (s <= t) ← これは入口と出口は入れ替えても移動時間が変わらないので考えやすいために制約をつける。
