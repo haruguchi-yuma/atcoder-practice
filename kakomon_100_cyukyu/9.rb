aa877468795990899b4c6f1ee864b37bbd73343c
yuma <ikeoku.y@gmail.com>
2024-12-17 22:28:47 +0900

    JOI 2008 予選 4 - 星座探し

--- kakomon_100_cyukyu/9.rb
+++ kakomon_100_cyukyu/9.rb
@@ -16,3 +16,26 @@ t.each do |x2,y2|
     break
   end
 end
+
+__END__
+# 2回目といたらほぼほぼ同じで笑った。
+m = gets.to_i
+t = Array.new(m) { gets.split.map(&:to_i) }
+n = gets.to_i
+s = Array.new(n) { gets.split.map(&:to_i) }
+
+points = {}
+s.each { |xy| points[xy] = true }
+
+target_x, target_y = t[0]
+
+s.each do |x, y|
+  diff_x = x - target_x
+  diff_y = y - target_y
+
+
+  if t.all? { |xx, yy| points[[xx+diff_x, yy+diff_y]] }
+    puts [diff_x, diff_y] * ' '
+  end
+end
+
