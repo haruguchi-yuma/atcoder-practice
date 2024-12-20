17461878b91ac584e31bfbfa3c76375e49b064c4
yuma <ikeoku.y@gmail.com>
2024-12-20 18:58:38 +0900

    JOI 2008 予選 5 - おせんべいを解いた

--- kakomon_100_cyukyu/13.rb
+++ kakomon_100_cyukyu/13.rb
@@ -10,3 +10,20 @@ ans = 0
 end
 
 p ans
+
+__END__
+# r が極端に小さいのでr でビット全探索する
+r, c = gets.split.map(&:to_i)
+a = Array.new(r) { gets.split.map(&:to_i) }
+
+ans = []
+
+(1<<r).times do |bit|
+  res = r.times.map do |i|
+    bit[i] == 1 ? a[i].map { _1 ^ 1 } : a[i]
+  end
+
+  ans << res.transpose.sum { x=_1.sum; [x, r-x].max }
+end
+
+p ans.max
