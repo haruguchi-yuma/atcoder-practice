9f64f877d84a3561420047d4cea1da7b2c94a935
haruguchi-yuma <ikeoku.y@gmail.com>
2026-09-11 21:06:34 +0900

    ABC370 D解き直し

--- abc/370/d/d.rb
+++ abc/370/d/d.rb
@@ -1,33 +1,58 @@
 require 'sorted_containers'
 
 h, w, q = gets.split.map(&:to_i)
-
-# まだ壊されてない壁の列、行を配列で保持しておく
 rows = Array.new(h) { SortedContainers::SortedArray.new(0...w) }
 cols = Array.new(w) { SortedContainers::SortedArray.new(0...h) }
 
-q.times do
-  i, j = gets.split.map { it.to_i - 1 }
-  walls = []
-
+q.times do |a|
+  i, j = gets.split.map(&:to_i).map(&:pred)
+  target = []
+  # クエリの座標に壁があった場合
   if rows[i].include?(j)
-    walls << [i, j]
+    rows[i].delete(j)
+    cols[j].delete(i)
   else
+    # クエリの座標に壁がなかった場合、上下左右を探す
+    # 範囲外にならないようにする
     right = rows[i].bisect_left(j)
     left = right - 1
-    walls << [i, rows[i][right]] if right < rows[i].size
-    walls << [i, rows[i][left]] if left >= 0
+    target << [i, rows[i][left]] if  left >= 0
+    target << [i, rows[i][right]] if right < rows[i].size
 
-   down = cols[j].bisect_left(i)
-   top = down - 1
-   walls << [cols[j][down], j] if down < cols[j].size
-   walls << [cols[j][top], j] if top >= 0
+
+    down = cols[j].bisect_left(i)
+    up = down - 1
+    target << [cols[j][up], j] if up >= 0
+    target << [cols[j][down], j] if down < cols[j].size
   end
 
-  walls.each do |wi, wj|
-    rows[wi].delete(wj)
-    cols[wj].delete(wi)
+  target.each do |di, dj|
+    rows[di].delete(dj)
+    cols[dj].delete(di)
   end
 end
 
-p rows.sum(&:size)
+puts rows.sum(&:size)
+__END__
+1 2
+1 2
+1 3
+
+0 1
+0 1
+0 2
+
+rows = [
+  [0, 2, 3],
+  [0, 1, 2, 3],
+]
+
+cols = [
+  [0, 1],
+  [1],
+  [0, 1],
+  [0, 1],
+]
+
+# x # #
+# # # #
